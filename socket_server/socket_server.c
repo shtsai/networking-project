a9dfe92417c518c5e5d98a95a6d1cbe77034b3c8
Shanghung Tsai <shtsai7@gmail.com>
2016-11-10 19:33:56 -0500

    clean socket_server.c and add comments

--- socket_server/socket_server.c
+++ socket_server/socket_server.c
@@ -18,60 +18,43 @@ char* skipEthHdr(char *buffer);
 int getProtocol(char *packet);
 void handleUDP(char *packet);
 void handleIPinIP(char *packet);
+void handleOSPF(char *packet);
 
 int main (int argc, char *argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "usage %s port\n", argv[0]);
-    exit(0);
-  }
-
   int sockfd;
-  int portno = atoi(argv[1]);
   struct sockaddr_in cli_addr, serv_addr;
   socklen_t clilen = sizeof(cli_addr);
   int recvlen;
-  
+
+  // initialize receive buffer
   char *buffer;
   buffer = (char *) malloc(2048);
   memset(buffer, 0, 2048);
 
-  sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_IP));
-  //  sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+  // create ethernet socket
+  // use ETH_P_IP will receive all IP packets to this server, along with their Ethernet headers
+  sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_IP)); 
   if (sockfd < 0) {
     error("ERROR socket");
   }
-
-  /* attemps to make TCP connection
-  serv_addr.sin_family = AF_INET;
-  // serv_addr.sin_addr.s_addr = INADDR_ANY;
-  if (inet_pton(AF_INET, "10.10.10.2", &serv_addr.sin_addr) <= 0) {
-    printf("inet_pton error occurred\n");
-    exit(0);
-  }
-  serv_addr.sin_port = htons(portno);
   
-  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-    error("ERROR on binding");
-  }
-  */
-  
-  /* set specific interface
+  /* set specific interface (OPTIONAL)
   if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, "enp0s8", strlen("enp0s8") + 1) < 0) {
     error("ERROR setting socket option");
   }
   */
 
-  int count = 0;
+  int count = 0;   // used for keeping track of the number of packets received
   while (1) {
+    printf("waiting for packet ...\n");
+    
     recvlen = recvfrom(sockfd, buffer, 2047, 0, (struct sockaddr *) &cli_addr, &clilen);
     if (recvlen > 0) {
       printf("Packet %d, %d bytes, ",count, recvlen);
-      //      printHex(buffer, recvlen);
 
-      char *packet = skipEthHdr(buffer);
-      int protocol = getProtocol(packet);
+      char *packet = skipEthHdr(buffer);  // all packets received contain ethernet headers, skip ethernet headers here
+      int protocol = getProtocol(packet); // this function reads the protocol value from the IP header
       printf("Protocol = %d\n", protocol);
-
 
       switch (protocol){	
       case 4:
@@ -80,22 +63,23 @@ int main (int argc, char *argv[]) {
       case 17:
 	handleUDP(packet);
 	break;
-	   /*
-	  case 6:
-	  handleTCP(packet);
-	  break;
-
-         case 89:
-	   handleOSPF(packet);
-	   break;
-	   */
+      case 89:  // handle OSPF here, haven't test it yet because no OSPF packet
+	handleOSPF(packet);
+	break;
+	
+      /*  // forget TCP packets for now, b/c need to deal with TCP handshakes
+      case 6:
+        handleTCP(packet);
+        break;
+      */
+	
       default:
 	printf("Other packet");
       }
 	
     }
-    memset(buffer, 0 ,2048);
-    recvlen = 0;
+    memset(buffer, 0 ,2048);    // reset receive buffer
+    recvlen = 0;               
     printf("\n");
     count++;
   }
@@ -106,34 +90,51 @@ int main (int argc, char *argv[]) {
 }
 
 char* skipEthHdr(char *buffer) {
+  // given a pointer to a received packet,
+  // this function skips its ethernet header
+  // and returns a pointer to the start of its IP header
+  
   int ethhdr_len = (int) sizeof(struct ethhdr);
   return buffer + ethhdr_len;
 }
 
 int getProtocol(char *packet) {
-  // input is a buffer of a received packet (Ethernet header included),
+  // given a pointer to a received packet (Ethernet header included),
   // this function returns the protocol number in the IP header
   struct ip *iphdr = (struct ip *) packet;
   return iphdr->ip_p;
 }
 
 void handleUDP(char *packet) {
+  // this function handles UDP packet by printing the information it contains
   int ip_len = printIPheader(packet);
   printUDPheader(packet, ip_len);
 }
 
 void handleIPinIP(char *packet) {
+  // this function handles IP-in-IP packet by printing the information it contains
+  
   // print outer header
   int ip_len1 = printIPheader(packet);
   
   int protocol = getProtocol(packet + ip_len1);
+
+  // only handles UDP packets for now, will add others
   if (protocol == 17) {
     handleUDP(packet + ip_len1);
   }
-  
+}
+
+void handleOSPF(char *packet) {
+  // this function handles OSPF packet
+  // for now it just prints a message, haven't test it yet
+  printf("This is a OSPF packet\n");
 }
 
 void printHex(char *buffer, int recvlen) {
+  // this function prints the packet in Hex value
+  // for debugging and testing purpose
+  
   printf("Here is the Hex format of the packet\n");
   for (int i = 0; i < recvlen; i++) {
     printf("%x ", buffer[i]);
