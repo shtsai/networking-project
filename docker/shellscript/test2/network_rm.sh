a9d28fe32d8072bd66fdc56c187bbff2a2d27667
Shang-Hung Tsai <st3127@nyu.edu>
2016-12-12 15:08:10 -0500

    add shellscript for test2

--- docker/shellscript/test2/network_rm.sh
+++ docker/shellscript/test2/network_rm.sh
@@ -2,5 +2,9 @@
 # This script removes all user-created docker subnets
 # Assume the following network names already exist in docker environment
 docker network rm vm1r1
-docker network rm r1r2
-docker network rm r2vm2
+docker network rm vm2r2
+docker network rm vm3r3
+docker network rm r1r3
+docker network rm r2r3
+docker network rm r1r4
+docker network rm r2r4
--- docker/shellscript/test2/remove_all.sh
+++ docker/shellscript/test2/remove_all.sh
@@ -2,10 +2,16 @@
 # This script first stops the following containers
 docker stop vm1
 docker stop vm2
+docker stop vm3
 docker stop r1
 docker stop r2
+docker stop r3
+docker stop r4
 # Then it removes them
 docker rm vm1
 docker rm vm2
+docker rm vm3
 docker rm r1
 docker rm r2
+docker rm r3
+docker rm r4
